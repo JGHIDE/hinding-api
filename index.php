f9fad9f21f620f7a557b26ad0eed1d73b4e42c54
JGHIDE <93085573+JGHIDE@users.noreply.github.com>
2021-10-25 10:47:57 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+function justgon1($ww) {
+mt_srand( (double)microtime() * 1000000000000000000 );
+$stt = array_rand($ww);
+return $ww[$stt];
+}
+function justgon($path) {
+$images = array();
+if ( $img_ww = @opendir($path) ) {
+while ( false !== ($img_www = readdir($img_ww)) ) {
+if ( preg_match("/(\.gif|\.jpg|\.png)$/", $img_www) ) {
+$images[] = $img_www;
+}
+}
+closedir($img_ww);
+}
+return $images;
+}
+$root = '';
+$path = 'uploads/anime/'; //tên thư mục chứa ảnh
+$list = justgon($root . $path);
+$link = 'https://api.vodaybubuoi.tk/';  //thay link relp vào
+$img = justgon1($list);
+$api = array(
+"data" => $link . $path . $img,
+"status" => "success",
+"author" => "JustGon"
+
+);
+$rdimg = json_encode($api, JSON_UNESCAPED_SLASHES);
+print($rdimg); 
+?>
